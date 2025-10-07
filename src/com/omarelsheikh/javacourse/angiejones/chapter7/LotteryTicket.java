bc5a4cd9c126033faf8578b6eaf8e27419dd8c5e
Omar Elsheikh <omar1999.elsheikh@gmail.com>
2025-10-07 08:16:50 +0300

    feat: add simple Lottery Ticket generator
    Added LotteryTicket class to generate and print random numbers.
    
    - Generates 6 random numbers between 1 and 6
    - Prints numbers with separators for readability
    - Included JavaDoc comments for methods and class

---
+++ src/com/omarelsheikh/javacourse/angiejones/chapter7/LotteryTicket.java
@@ -0,0 +1,38 @@
+package com.omarelsheikh.javacourse.angiejones.chapter7;
+
+import java.util.Random;
+
+/**
+ * A simple lottery ticket generator.
+ * Generates a set of 6 random numbers and prints them to the console.
+ */
+public class LotteryTicket {
+
+    public static final int LENGTH = 6;
+    public static final int MAX_TICKET_NUMBER = 6;
+
+    public static void main(String[] args) {
+
+        int[] ticketNumbers = generateNumbers();
+        printTicketNumbers(ticketNumbers);
+    }
+
+    // Generate 6 random numbers between 1 and 6
+    public static int[] generateNumbers() {
+
+        int[] ticketNumbers = new int[LENGTH];
+        Random random = new Random();
+
+        for (int i = 0; i < LENGTH; i++) {
+            ticketNumbers[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+        }
+        return ticketNumbers;
+    }
+
+    // Print the numbers in the array
+    public static void printTicketNumbers(int[] ticketNumbers) {
+        for (int i = 0; i < LENGTH; i++) {
+            System.out.print(ticketNumbers[i] + " | ");
+        }
+    }
+}
